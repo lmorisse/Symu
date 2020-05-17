9ac87224762193536a027ea0745c9325906e6b73
Laurent Morisseau <laurent.morisseau@gmail.com>
2020-05-17 21:46:21 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,10 +51,7 @@ exclude_trees = ['_build']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'basic'
-html_theme_options = {
-    "navigation_with_keys": "true"
-}
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
